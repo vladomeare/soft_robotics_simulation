a02c2283c3acf0554841197eaffb5187b685f8f2
Vladomeare <vladomeare@gmail.com>
2023-11-14 11:52:32 +0100

    Changed laplacian

--- main.m
+++ main.m
@@ -17,10 +17,10 @@ lambda = 0.5;
 [x,y] = random_points_on_circle(n,r);
 p = [x'; y'];
 L_1D = laplace_matrix(n);
+L = [L_1D, zeros(n); zeros(n), L_1D];
 
 figure(1);
 
-%L = [L_1D, zeros(n); zeros(n), L_1D];
 subplot(2,2,1);
 full_round_x = [x'; x(1)];
 full_round_y = [y'; y(1)];
@@ -29,9 +29,7 @@ subplot(2,2,2);
 f2 = plot(x',y','.');
 subplot(2,2,3);
 for i = 1:1:iter
-   temp_x = p(1:n)'*L_1D;
-   temp_y = p(n+1:2*n)'*L_1D;
-   p = p + lambda*[temp_x';temp_y']; 
+   p = p + lambda*L*p; 
    f3 = plot(p(1:n),p(n+1:2*n));
    title("iteratie " + string(i));
    pause(1)
